00ac4efe9a138861dd02ce0a387ece37ac7aa58e
Birowsky <birowsky@gmail.com>
2018-01-08 18:20:08 +0100

    import specific function

--- src/external1.ts
+++ src/external1.ts
@@ -1,4 +1,4 @@
-import * as External2 from './external2';
+import {function1 as f1} from './external2';
 
 export {
   function1,
@@ -9,5 +9,5 @@ const function1 = () =>
   console.log('external1 function1');
 
 const function2 = () =>
-  console.log('external1 function2', External2.function1);
+  console.log('external1 function2', f1);
 
